284799770656d0843aa59471b425d39ad8e09c37
edgaroc30 <edgar.ochoa.c@gmail.com>
2022-05-05 11:12:00 +0200

    Revert "adding keysets used for views"
    This reverts commit 2a2e4eafd2aa4c419fc398efc25d62d3b02dce46.

--- flycs_sdk/views.py
+++ flycs_sdk/views.py
@@ -1,6 +1,6 @@
 """Module containing view classes."""
 
-from typing import Optional
+from typing import Optional, List
 
 from flycs_sdk.custom_code import Dependency
 from flycs_sdk.query_base import QueryBase
@@ -20,6 +20,7 @@ class View(QueryBase):
         encrypt: Optional[bool] = None,
         static: Optional[bool] = True,
         destination_data_mart: Optional[str] = None,
+        keysets_used: Optional[List[str]] = None,
     ):
         """Create a View object.
 
@@ -48,7 +49,7 @@ class View(QueryBase):
         self.destination_table = None
         self.dependencies = []
         self.parsing_dependencies = []
-        self.keysets_used = []
+        self.keysets_used = keysets_used or []
 
     @classmethod
     def from_dict(cls, d: dict):
@@ -67,13 +68,13 @@ class View(QueryBase):
             encrypt=d.get("ENCRYPT", None),
             static=d.get("STATIC", True),
             destination_data_mart=d.get("DESTINATION_DATA_MART"),
+            keysets_used=d.get("KEYSETS_USED", []),
         )
         view.destination_table = d.get("DESTINATION_TABLE")
         view.dependencies = [Dependency.from_dict(x) for x in d.get("DEPENDS_ON") or []]
         view.parsing_dependencies = [
             Dependency.from_dict(x) for x in d.get("PARSING_DEPENDS_ON") or []
         ]
-        view.keysets_used = d.get("KEYSETS_USED") or []
         return view
 
     def to_dict(self) -> dict:
